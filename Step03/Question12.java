0d15cfbfa38bbba217b36fa4a2e17fa0522c33ab
asdf-qw-er <asdfqwer0113@gmail.com>
2024-08-18 00:19:22 +0900

    Commit Step03

--- Step03/Question12.java
+++ Step03/Question12.java
@@ -3,20 +3,20 @@ package Step03;
 import java.util.Scanner;
 
 public class Question12 {
-	
+
 	private void Function() {
 		try (Scanner scanner = new Scanner(System.in)) {
 			while (true) {
-				int num1=scanner.nextInt();
-				int num2=scanner.nextInt();
-				
-				System.out.println(num1+num2);
+				int num1 = scanner.nextInt();
+				int num2 = scanner.nextInt();
+
+				System.out.println(num1 + num2);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		Question12 q12=new Question12();
+		Question12 q12 = new Question12();
 		q12.Function();
 	}
 }
